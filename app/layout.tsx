e53fc299727b4e50c95317f8b8946a5b2b99bec3
jeffkhong11 <122283133+jeffkhong11@users.noreply.github.com>
2024-05-09 23:29:50 +0800

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import { ThemeProvider } from "next-themes"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -18,9 +19,11 @@ export default function RootLayout({
       <head />
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} attribute="class">
-          <Navbar />
-          {children}
-          <Footer />
+          <Analytics>
+            <Navbar />
+            {children}
+            <Footer />
+          </Analytics>
         </ThemeProvider>
       </body>
     </html>
